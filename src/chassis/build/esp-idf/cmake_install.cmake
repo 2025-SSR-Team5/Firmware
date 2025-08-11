1994f621164c4c08bd5efb6a3b9c785b508590c0
issaimaru <takeuchiissai2355@gmail.com>
2025-08-12 00:47:50 +0900

    add: 三輪オムニのExampleコードを作成(オシロである程度動作確認済み)

--- src/chassis/build/esp-idf/cmake_install.cmake
+++ src/chassis/build/esp-idf/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/issaimaru/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp32-elf-objdump")
+  set(CMAKE_OBJDUMP "/home/issaimaru/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
@@ -534,6 +534,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("/home/issaimaru/esp/2025-SSR-Team5/Firmware/src/chassis/build/esp-idf/touch_element/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("/home/issaimaru/esp/2025-SSR-Team5/Firmware/src/chassis/build/esp-idf/ulp/cmake_install.cmake")
 endif()
 
@@ -545,6 +550,11 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
   include("/home/issaimaru/esp/2025-SSR-Team5/Firmware/src/chassis/build/esp-idf/wifi_provisioning/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/issaimaru/esp/2025-SSR-Team5/Firmware/src/chassis/build/esp-idf/espressif__bdc_motor/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
